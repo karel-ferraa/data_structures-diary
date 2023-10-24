c34a8a4a5f017d27357235932129fb9dbe5f002d
Anatole Sot <anatolesot@gmail.com>
2023-10-24 17:41:49 +0200

    Switched up the prototype of the createList func

--- part1/list.h
+++ part1/list.h
@@ -1,16 +1,19 @@
 #ifndef LIST_H
 #define LIST_H
 
+
 typedef struct s_cell {
     int val;
+    int level;
     struct s_cell** next;
 } cell;
 
 typedef struct s_list {
     cell* head;
+    int maxLevel=5;
 } list;
 
-list* createList();
+list* createList(int maxlevel);
 
 void addElement(list* l, int val);
 
