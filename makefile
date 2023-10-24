4fb60b86f9931192ce2bfb585b4a26497096239b
Karel BOU FERRAA <karel.bou-ferraa@efrei.net>
2023-10-24 18:08:07 +0200

    feat(main.c, makefile): add makefile, basic main.c
    main.c creates a one-level list, displays it, populates it and displays
    it again

---
+++ makefile
@@ -0,0 +1,2 @@
+diary:
+	$(CC) -Wall part1/main.c part1/list.c part1/list.h -o part1/diary
--- part1/main.c
+++ part1/main.c
@@ -1,4 +1,18 @@
 int main(){
-    printf("Hello World!\n");
-    return 0;
+	s_list list;
+	list = createList(1);
+	displayLevelledList(list);
+
+	insertElement(list, 18);
+	insertElement(list, 25);
+	insertElement(list, 31);
+	insertElement(list, 32);
+	insertElement(list, 56);
+	insertElement(list, 59);
+	insertElement(list, 59);
+	insertElement(list, 91);
+
+	displayLevelledList(list);
+
+	return 0;
 }
