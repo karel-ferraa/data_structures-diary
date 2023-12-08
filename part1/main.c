99213f99847cbb321eb817927ccd60f94b0ec3cb
Karel BOU FERRAA <karel.bou-ferraa@efrei.net>
2023-12-08 11:00:20 +0100

    feat(main.c): simple structure for a menu prompt

--- part1/main.c
+++ part1/main.c
@@ -1,5 +1,6 @@
 #include "calendar.h"
 #include "list.h"
+#include "levellist.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -69,5 +70,59 @@ int verifyContact(contact c){
 	return 1;
 }
 
+int menu() {
+	char choice = 'a';
 
-	
+	// 48 and 55 are ascii values for 0 and 7
+	while (choice < 48 || choice > 55) {
+		printf("Select the action to perform by typing in the corresponding number\n");
+		printf("0. Quit the program");
+		printf("1. Search for a contact\n");
+		printf("2. View a contact's appointments\n");
+		printf("3. Create a contact\n");
+		printf("4. Create an appointment for a contact\n");
+		printf("5. Delete an appointment\n");
+		printf("6. Save the file of all appointments\n");
+		printf("7. Load the file of all appointments\n");
+
+		scanf("%c", &choice);
+	}
+	return choice;
+}
+
+int main() {
+	int choice;
+	while (choice != '0') {
+		choice = menu();
+		switch (choice) {
+			case '1':
+			{
+					printf("Search is not yet implemented");
+			}
+			case '2':
+			{
+					printf("Viewing a contact's appointments is not yet implemented");
+			}
+			case '3':
+			{
+					printf("Creating a contact is not yet implemented");
+			}
+			case '4':
+			{
+					printf("Creating a appointment for a contact is not yet implemented");
+			}
+			case '5':
+			{
+					printf("Deleting an appointment is not yet implemented");
+			}
+			case '6':
+			{
+					printf("Saving the file of all appointments is not yet implemented");
+			}
+			case '7':
+			{
+					printf("Loading the file of all appointments is not yet implemented");
+			}
+		}
+	}
+}
